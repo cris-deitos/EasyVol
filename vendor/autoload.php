4c9160c9490b40bc42cc49c8d9de43749ff2a7d6
cris-deitos <cristian.93.ebay@gmail.com>
2025-12-07 18:43:04 +0100

    Refactor autoloader for PHPMailer and dependencies

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -2,50 +2,47 @@
 /**
  * Manual Autoloader for EasyVol
  * 
- * This autoloader handles PHPMailer, mPDF, PhpSpreadsheet, QR Code, and reCAPTCHA
+ * This autoloader handles PHPMailer 7.x, mPDF, PhpSpreadsheet, QR Code, and reCAPTCHA
  * libraries when uploaded via FTP without Composer.
  * 
  * @package EasyVol
  * @author cris-deitos
- * @created 2025-12-07
+ * @updated 2025-12-07 - PHPMailer 7.x support
  */
 
 // Define vendor directory
-define('VENDOR_DIR', __DIR__);
+if (!defined('VENDOR_DIR')) {
+    define('VENDOR_DIR', __DIR__);
+}
 
 /**
- * SPL Autoloader for namespace-based classes
+ * PSR-4 Autoloader for namespace-based classes
+ * Handles PHPMailer 7.x, Endroid, Google reCAPTCHA
  */
 spl_autoload_register(function ($class) {
-    // Normalize namespace separator
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    
-    // Array of possible library paths
-    $paths = [
-        // PHPMailer
-        VENDOR_DIR . '/phpmailer/phpmailer/src/' . $class . '.php',
-        
-        // mPDF
-        VENDOR_DIR . '/mpdf/mpdf/src/' . $class . '.php',
-        
-        // PhpSpreadsheet
-        VENDOR_DIR . '/phpoffice/phpspreadsheet/src/' . $class . '.php',
-        
-        // QR Code (endroid)
-        VENDOR_DIR . '/endroid/qr-code/src/' . $class . '.php',
-        
-        // QR Code (chillerlan)
-        VENDOR_DIR . '/chillerlan/php-qrcode/src/' . $class . '.php',
-        
-        // reCAPTCHA
-        VENDOR_DIR . '/google/recaptcha/src/' . $class . '.php',
-        
-        // Generic PSR-4 style path
-        VENDOR_DIR . '/' . $class . '.php',
+    // Map of namespace prefixes to base directories
+    $prefixes = [
+        'PHPMailer\\PHPMailer\\' => VENDOR_DIR . '/phpmailer/phpmailer/src/',
+        'Endroid\\QrCode\\' => VENDOR_DIR . '/endroid/qr-code/src/',
+        'ReCaptcha\\' => VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/',
+        'Mpdf\\' => VENDOR_DIR . '/mpdf/mpdf/src/',
+        'PhpOffice\\PhpSpreadsheet\\' => VENDOR_DIR . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/',
     ];
     
-    // Try to load the class from available paths
-    foreach ($paths as $file) {
+    // Check each namespace prefix
+    foreach ($prefixes as $prefix => $baseDir) {
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            continue;
+        }
+        
+        // Get the relative class name
+        $relativeClass = substr($class, $len);
+        
+        // Replace namespace separators with directory separators
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+        
+        // If the file exists, require it
         if (file_exists($file)) {
             require_once $file;
             return true;
@@ -56,168 +53,190 @@ spl_autoload_register(function ($class) {
 });
 
 /**
- * PHPMailer Manual Autoloader
+ * PHPMailer 7.x Manual Loader
+ * Ensures all core PHPMailer classes are loaded
  */
-if (file_exists(VENDOR_DIR . '/phpmailer/phpmailer/src/PHPMailer.php')) {
-    require_once VENDOR_DIR . '/phpmailer/phpmailer/src/Exception.php';
-    require_once VENDOR_DIR . '/phpmailer/phpmailer/src/PHPMailer.php';
-    require_once VENDOR_DIR . '/phpmailer/phpmailer/src/SMTP.php';
-    
-    // Optional PHPMailer components
-    if (file_exists(VENDOR_DIR . '/phpmailer/phpmailer/src/OAuth.php')) {
-        require_once VENDOR_DIR . '/phpmailer/phpmailer/src/OAuth.php';
-    }
-    if (file_exists(VENDOR_DIR . '/phpmailer/phpmailer/src/POP3.php')) {
-        require_once VENDOR_DIR . '/phpmailer/phpmailer/src/POP3.php';
-    }
-}
-
-/**
- * mPDF Manual Autoloader
- */
-if (file_exists(VENDOR_DIR . '/mpdf/mpdf/src/Mpdf.php')) {
-    require_once VENDOR_DIR . '/mpdf/mpdf/src/Mpdf.php';
-    
-    // Load mPDF dependencies if they exist
-    $mpdfDirs = [
-        '/mpdf/mpdf/src',
-        '/mpdf/mpdf/src/Config',
-        '/mpdf/mpdf/src/Conversion',
-        '/mpdf/mpdf/src/Writer',
-        '/mpdf/mpdf/src/Pdf',
+$phpmailerSrc = VENDOR_DIR . '/phpmailer/phpmailer/src/';
+if (file_exists($phpmailerSrc . 'PHPMailer.php')) {
+    // Core classes - load in dependency order
+    $phpmailerClasses = [
+        'Exception. php',
+        'PHPMailer.php',
+        'SMTP.php',
     ];
     
-    foreach ($mpdfDirs as $dir) {
-        $fullPath = VENDOR_DIR . $dir;
-        if (is_dir($fullPath)) {
-            foreach (glob($fullPath . '/*.php') as $file) {
-                require_once $file;
-            }
+    foreach ($phpmailerClasses as $class) {
+        $file = $phpmailerSrc . $class;
+        if (file_exists($file)) {
+            require_once $file;
         }
     }
-}
-
-/**
- * PhpSpreadsheet Manual Autoloader
- */
-if (file_exists(VENDOR_DIR . '/phpoffice/phpspreadsheet/src/Bootstrap.php')) {
-    require_once VENDOR_DIR . '/phpoffice/phpspreadsheet/src/Bootstrap.php';
-} elseif (is_dir(VENDOR_DIR . '/phpoffice/phpspreadsheet/src')) {
-    // Alternative: Load main PhpSpreadsheet classes
-    $spreadsheetFiles = [
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Spreadsheet.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/IOFactory.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Writer/IWriter.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Writer/Xlsx.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Writer/Csv.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/IReader.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/Xlsx.php',
-        '/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/Csv.php',
+    
+    // Optional PHPMailer 7.x components
+    $optionalClasses = [
+        'OAuth.php',
+        'POP3.php',
+        'DSNConfigurator.php',
     ];
     
-    foreach ($spreadsheetFiles as $file) {
-        $fullPath = VENDOR_DIR . $file;
-        if (file_exists($fullPath)) {
-            require_once $fullPath;
-        }
-    }
-}
-
-/**
- * QR Code Library Manual Autoloader (endroid/qr-code)
- */
-if (file_exists(VENDOR_DIR . '/endroid/qr-code/src/QrCode.php')) {
-    require_once VENDOR_DIR . '/endroid/qr-code/src/QrCode.php';
-    
-    // Load additional QR code classes if they exist
-    $qrCodeDirs = [
-        '/endroid/qr-code/src',
-        '/endroid/qr-code/src/Writer',
-    ];
-    
-    foreach ($qrCodeDirs as $dir) {
-        $fullPath = VENDOR_DIR . $dir;
-        if (is_dir($fullPath)) {
-            foreach (glob($fullPath . '/*.php') as $file) {
-                require_once $file;
-            }
-        }
-    }
-}
-
-/**
- * QR Code Library Manual Autoloader (chillerlan/php-qrcode)
- */
-if (file_exists(VENDOR_DIR . '/chillerlan/php-qrcode/src/QRCode.php')) {
-    require_once VENDOR_DIR . '/chillerlan/php-qrcode/src/QRCode.php';
-    
-    // Load additional classes
-    if (is_dir(VENDOR_DIR . '/chillerlan/php-qrcode/src')) {
-        foreach (glob(VENDOR_DIR . '/chillerlan/php-qrcode/src/*.php') as $file) {
+    foreach ($optionalClasses as $class) {
+        $file = $phpmailerSrc . $class;
+        if (file_exists($file)) {
             require_once $file;
         }
     }
 }
 
 /**
- * Google reCAPTCHA Manual Autoloader
+ * Endroid QR Code Loader
  */
-if (file_exists(VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/ReCaptcha.php')) {
-    require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/ReCaptcha.php';
-    require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod.php';
-    require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestParameters.php';
-    require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/Response.php';
+$endroidSrc = VENDOR_DIR . '/endroid/qr-code/src/';
+if (is_dir($endroidSrc)) {
+    // Try to load main QrCode class
+    if (file_exists($endroidSrc . 'QrCode. php')) {
+        require_once $endroidSrc . 'QrCode.php';
+    }
     
-    // Load request methods
-    if (file_exists(VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/Post.php')) {
-        require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/Post.php';
-    }
-    if (file_exists(VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/Curl.php')) {
-        require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/Curl.php';
-    }
-    if (file_exists(VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/CurlPost.php')) {
-        require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/CurlPost.php';
-    }
-    if (file_exists(VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/Socket.php')) {
-        require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/Socket.php';
-    }
-    if (file_exists(VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/SocketPost.php')) {
-        require_once VENDOR_DIR . '/google/recaptcha/src/ReCaptcha/RequestMethod/SocketPost.php';
+    // Load common classes
+    $endroidClasses = [
+        'Builder/Builder.php',
+        'Builder/BuilderInterface.php',
+        'Color/Color.php',
+        'Color/ColorInterface.php',
+        'Encoding/Encoding.php',
+        'Encoding/EncodingInterface.php',
+        'ErrorCorrectionLevel/ErrorCorrectionLevel.php',
+        'ErrorCorrectionLevel/ErrorCorrectionLevelInterface.php',
+        'Label/Label.php',
+        'Label/LabelInterface.php',
+        'Logo/Logo.php',
+        'Logo/LogoInterface.php',
+        'RoundBlockSizeMode/RoundBlockSizeMode. php',
+        'RoundBlockSizeMode/RoundBlockSizeModeInterface.php',
+        'Writer/WriterInterface.php',
+        'Writer/Result/ResultInterface.php',
+    ];
+    
+    foreach ($endroidClasses as $class) {
+        $file = $endroidSrc . $class;
+        if (file_exists($file)) {
+            require_once $file;
+        }
     }
 }
 
 /**
- * Additional common dependencies autoloader
+ * Google reCAPTCHA Loader
  */
-$commonDependencies = [
-    // PSR interfaces
-    '/psr/log/Psr/Log/LoggerInterface.php',
-    '/psr/http-message/src/MessageInterface.php',
-    '/psr/http-client/src/ClientInterface.php',
+$recaptchaSrc = VENDOR_DIR .  '/google/recaptcha/src/ReCaptcha/';
+if (file_exists($recaptchaSrc . 'ReCaptcha. php')) {
+    $recaptchaClasses = [
+        'ReCaptcha. php',
+        'RequestMethod. php',
+        'RequestParameters. php',
+        'Response.php',
+        'RequestMethod/Post.php',
+        'RequestMethod/Curl.php',
+        'RequestMethod/CurlPost.php',
+        'RequestMethod/Socket. php',
+        'RequestMethod/SocketPost.php',
+    ];
     
-    // Symfony components (often used by libraries)
-    '/symfony/polyfill-mbstring/bootstrap.php',
-    '/symfony/polyfill-php72/bootstrap.php',
-    '/symfony/polyfill-php80/bootstrap.php',
+    foreach ($recaptchaClasses as $class) {
+        $file = $recaptchaSrc . $class;
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+}
+
+/**
+ * mPDF Loader (if needed)
+ * Note: mPDF has many dependencies, best installed via Composer
+ */
+$mpdfSrc = VENDOR_DIR . '/mpdf/mpdf/src/';
+if (file_exists($mpdfSrc .  'Mpdf.php')) {
+    // Only load if mPDF vendor autoload exists
+    $mpdfAutoload = VENDOR_DIR . '/mpdf/mpdf/vendor/autoload.php';
+    if (file_exists($mpdfAutoload)) {
+        require_once $mpdfAutoload;
+    }
+    
+    // Load main Mpdf class
+    require_once $mpdfSrc .  'Mpdf.php';
+}
+
+/**
+ * PhpSpreadsheet Loader (if needed)
+ * Note: PhpSpreadsheet has many dependencies, best installed via Composer
+ */
+$spreadsheetSrc = VENDOR_DIR .  '/phpoffice/phpspreadsheet/src/';
+if (is_dir($spreadsheetSrc)) {
+    // Check for Bootstrap file
+    if (file_exists($spreadsheetSrc . 'Bootstrap.php')) {
+        require_once $spreadsheetSrc . 'Bootstrap.php';
+    }
+    
+    // Load vendor autoload if exists
+    $spreadsheetAutoload = VENDOR_DIR . '/phpoffice/phpspreadsheet/vendor/autoload.php';
+    if (file_exists($spreadsheetAutoload)) {
+        require_once $spreadsheetAutoload;
+    }
+}
+
+/**
+ * PSR Interfaces Loader (if present)
+ */
+$psrInterfaces = [
+    '/psr/log/Psr/Log/LoggerInterface.php',
+    '/psr/http-message/src/MessageInterface. php',
+    '/psr/http-client/src/ClientInterface.php',
 ];
 
-foreach ($commonDependencies as $file) {
-    $fullPath = VENDOR_DIR . $file;
-    if (file_exists($fullPath)) {
-        require_once $fullPath;
+foreach ($psrInterfaces as $interface) {
+    $file = VENDOR_DIR . $interface;
+    if (file_exists($file)) {
+        require_once $file;
     }
 }
 
 /**
- * Class alias helper for common use cases
+ * Symfony Polyfills Loader (if present)
  */
-if (!class_exists('PHPMailer') && class_exists('PHPMailer\PHPMailer\PHPMailer')) {
-    class_alias('PHPMailer\PHPMailer\PHPMailer', 'PHPMailer');
-    class_alias('PHPMailer\PHPMailer\Exception', 'PHPMailerException');
-    class_alias('PHPMailer\PHPMailer\SMTP', 'SMTP');
+$polyfills = [
+    '/symfony/polyfill-mbstring/bootstrap. php',
+    '/symfony/polyfill-php72/bootstrap. php',
+    '/symfony/polyfill-php80/bootstrap. php',
+    '/symfony/polyfill-ctype/bootstrap.php',
+    '/symfony/polyfill-iconv/bootstrap.php',
+];
+
+foreach ($polyfills as $polyfill) {
+    $file = VENDOR_DIR . $polyfill;
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+
+/**
+ * Class Aliases for backward compatibility
+ */
+if (class_exists('PHPMailer\PHPMailer\PHPMailer')) {
+    if (! class_exists('PHPMailer', false)) {
+        class_alias('PHPMailer\PHPMailer\PHPMailer', 'PHPMailer');
+    }
+    if (!class_exists('PHPMailerException', false)) {
+        class_alias('PHPMailer\PHPMailer\Exception', 'PHPMailerException');
+    }
+    if (!class_exists('SMTP', false)) {
+        class_alias('PHPMailer\PHPMailer\SMTP', 'SMTP');
+    }
 }
 
 // Success indicator
-define('VENDOR_AUTOLOAD_LOADED', true);
+if (!defined('VENDOR_AUTOLOAD_LOADED')) {
+    define('VENDOR_AUTOLOAD_LOADED', true);
+}
 
+// Return true to indicate successful load
 return true;
