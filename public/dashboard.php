faa8cd1651204776f446d7f5093d328d8e54ffe4
cris-deitos <cristian.93.ebay@gmail.com>
2025-12-08 02:48:57 +0100

    Update script source path for main.js

--- public/dashboard.php
+++ public/dashboard.php
@@ -318,6 +318,6 @@ AutoLogger::logPageAccess();
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/main.js"></script>
+    <script src="../assets/js/main.js"></script>
 </body>
 </html>
