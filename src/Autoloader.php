e2c777adb8bfcf9d0d33c46beb717e46aaec3b74
cris-deitos <cristian.93.ebay@gmail.com>
2025-12-08 03:46:59 +0100

    Enhance autoloader with vendor folder validation
    Updated autoloader to check for complete vendor folder before loading.

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,4 +1,4 @@
-<?php
+<? php
 namespace EasyVol;
 
 /**
@@ -13,14 +13,19 @@ class Autoloader {
         }
         
         // Load Composer's vendor autoloader for third-party dependencies
-        $vendorAutoload = __DIR__ . '/../vendor/autoload.php';
-        if (file_exists($vendorAutoload)) {
+        // BUT ONLY if the vendor folder is complete (not just autoload.php)
+        $vendorAutoload = __DIR__ .  '/../vendor/autoload.php';
+        $vendorComposer = __DIR__ . '/../vendor/composer/autoload_real.php';
+        
+        if (file_exists($vendorAutoload) && file_exists($vendorComposer)) {
+            // Vendor folder is complete - load it
             require_once $vendorAutoload;
         } else {
-            // Log warning if vendor autoloader is missing
-            error_log('WARNING: Composer vendor autoloader not found at ' . $vendorAutoload . 
-                      '. Please run "composer install" to install dependencies. ' .
-                      'Features requiring third-party libraries (email, PDF, Excel) will not work.');
+            // Vendor folder missing or incomplete - skip it
+            // This is OK - TCPDF and other libs are loaded manually if needed
+            error_log('INFO: Composer vendor autoloader skipped (folder incomplete).  ' .
+                      'This is normal if Composer was not used.  ' .
+                      'TCPDF and custom classes will be loaded via custom autoloader.');
         }
         
         spl_autoload_register(function ($class) {
@@ -39,7 +44,7 @@ class Autoloader {
             $relativeClass = substr($class, $len);
             
             // Replace namespace separators with directory separators
-            $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+            $file = $baseDir . str_replace('\\', '/', $relativeClass) . '. php';
             
             // If the file exists, require it
             if (file_exists($file)) {
