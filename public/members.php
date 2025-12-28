6df3d823a2f03d05d038898388be5648d48c66e3
cris-deitos <cristian.93.ebay@gmail.com>
2025-12-28 16:21:15 +0100

    Fix photo path check for member photos

--- public/members.php
+++ public/members.php
@@ -247,7 +247,7 @@ $pageTitle = 'Gestione Soci';
                                         <?php foreach ($members as $member): ?>
                                             <tr>
                                                 <td>
-                                                    <?php if (!empty($member['photo_path']) && file_exists(PathHelper::relativeToAbsolute($member['photo_path']))): ?>
+                                                    <?php if (!empty($member['photo_path']) && file_exists($member['photo_path'])): ?>
                                                         <img src="download.php?type=member_photo&id=<?php echo $member['id']; ?>" 
                                                              alt="Foto" class="rounded-circle" style="width: 40px; height: 40px; object-fit: cover;">
                                                     <?php else: ?>
