07ac052f40f77ced0b73fe312d5f4133154572d2
cris-deitos <cristian.93.ebay@gmail.com>
2026-01-04 13:15:24 +0100

    Update warehouse_view.php

--- public/warehouse_view.php
+++ public/warehouse_view.php
@@ -62,7 +62,6 @@ $pageTitle = 'Dettaglio Articolo: ' . $item['name'];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../assets/css/main.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 </head>
