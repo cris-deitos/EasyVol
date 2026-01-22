d55481c322731760015117cf7262f8b4fc9c30fe
cris-deitos <cristian.93.ebay@gmail.com>
2026-01-22 11:06:21 +0100

    Remove export report button from dashboard
    Removed export report button from the dashboard.

--- public/dashboard.php
+++ public/dashboard.php
@@ -85,13 +85,6 @@ AutoLogger::logPageAccess();
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2"><i class="bi bi-speedometer2"></i> Dashboard</h1>
-                    <div class="btn-toolbar mb-2 mb-md-0">
-                        <div class="btn-group me-2">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">
-                                <i class="bi bi-download"></i> Esporta Report
-                            </button>
-                        </div>
-                    </div>
                 </div>
 
                 <!-- Statistics Cards -->
